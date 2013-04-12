f078e3e00f18f36e705a6bdfe5b34c8daf15a116
Maksim Terentyev <maksim@MacBook-Pro-Maksim-Terentyev.local>
2013-04-12 20:20:07 +0400

    Никаких констант в коде

--- src/com/github/charslush/second/main/Application.java
+++ src/com/github/charslush/second/main/Application.java
@@ -10,23 +10,28 @@ import java.io.IOException;
 
 public class Application {
 
+    private static final String JPG = "jpg";
+    private static final String HTML = "html";
+    private static final String TXT = "txt";
+    private static final String DOT = ".";
+
     public static void main(String[] args) throws IOException {
         Copy copyObject = new Html("");
 
         if (args.length > 0) {
             String enterString = args[0];
-            int dotPos = enterString.lastIndexOf(".");
+            int dotPos = enterString.lastIndexOf(DOT);
             String ext = enterString.substring(dotPos);
-            if (ext.equals("jpg")) {
+            if (ext.equals(JPG)) {
                 Copy pic = new Pictures(enterString);
                 pic.open();
             }
-            if (ext.equals("html")) {
+            if (ext.equals(HTML)) {
                 Copy html = new Html(enterString);
                 html.open();
             }
 
-            if (ext.equals("txt")) {
+            if (ext.equals(TXT)) {
                 Copy txt = new Text(enterString);
                 txt.open();
             } else {
