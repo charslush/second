2add6809121db2fec5241358a9abdfb7d7b4a952
Maksim Terentyev <maksim@MacBook-Pro-Maksim-Terentyev.local>
2013-04-12 20:28:06 +0400

    Метод copy  в классах одинаковый - надо убирать в предка

--- src/com/github/charslush/second/Copy.java
+++ src/com/github/charslush/second/Copy.java
@@ -1,8 +1,16 @@
 package com.github.charslush.second;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
 
 public abstract class Copy {
+
+    private static final String SLASH = "/";
+    private static final String DOT = ".";
+    private static final String EXT = "";
 
     protected String url;
     protected int count;
@@ -12,7 +20,17 @@ public abstract class Copy {
         this.count++;
     }
 
-    public abstract void save() throws IOException;
+    public void save() throws IOException {
+        int slashPos = url.lastIndexOf(SLASH);
+        String name = url.substring(slashPos);
+        int dotPos = url.lastIndexOf(DOT);
+        String ext = url.substring(dotPos);
+        String adress = "~/html" + name;
+        URL website = new URL(url);
+        ReadableByteChannel rbc = Channels.newChannel(website.openStream());
+        FileOutputStream fos = new FileOutputStream(adress);
+        fos.getChannel().transferFrom(rbc, 0, 1 << 24);
+    }
 
     public abstract void open() throws IOException;
 }
--- src/com/github/charslush/second/Html.java
+++ src/com/github/charslush/second/Html.java
@@ -1,10 +1,6 @@
 package com.github.charslush.second;
 
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.net.URL;
-import java.nio.channels.Channels;
-import java.nio.channels.ReadableByteChannel;
 
 
 public class Html extends Copy {
@@ -15,15 +11,6 @@ public class Html extends Copy {
     }
 
     public void save() throws IOException {
-        int slashPos = url.lastIndexOf("/");
-        String name = url.substring(slashPos);
-        int dotPos = url.lastIndexOf(".");
-        String ext = url.substring(dotPos);
-        String adress = "~/html" + name;
-        URL website = new URL(url);
-        ReadableByteChannel rbc = Channels.newChannel(website.openStream());
-        FileOutputStream fos = new FileOutputStream(adress);
-        fos.getChannel().transferFrom(rbc, 0, 1 << 24);
     }
 
     public void open() throws IOException {
