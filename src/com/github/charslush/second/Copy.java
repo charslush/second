31982974167ee4a5f0fc1f571fde39b07d139319
Maksim Terentyev <maksim@MacBook-Pro-Maksim-Terentyev.local>
2013-04-12 20:36:47 +0400

    Выносим константы в отдельный файл

--- src/com/github/charslush/second/Copy.java
+++ src/com/github/charslush/second/Copy.java
@@ -1,5 +1,7 @@
 package com.github.charslush.second;
 
+import com.github.charslush.second.consts.Constants;
+
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URL;
@@ -8,26 +10,24 @@ import java.nio.channels.ReadableByteChannel;
 
 public abstract class Copy {
 
-    private static final String SLASH = "/";
-    private static final String DOT = ".";
-    private static final String EXT = "";
+    private final String ext;
     private final String type;
-
     protected String url;
     protected int count;
 
-    public Copy(String type, String url) {
+    public Copy(String ext, String type, String url) {
+        this.ext = ext;
         this.type = type;
         this.url = url;
         this.count++;
     }
 
     public void save() throws IOException {
-        int slashPos = url.lastIndexOf(SLASH);
+        int slashPos = url.lastIndexOf(Constants.SLASH);
         String name = url.substring(slashPos);
-        int dotPos = url.lastIndexOf(DOT);
+        int dotPos = url.lastIndexOf(Constants.DOT);
         String ext = url.substring(dotPos);
-        String adress = "~/" + type + name;
+        String adress = Constants.DESTINATION_DIR + type + name;
         URL website = new URL(url);
         ReadableByteChannel rbc = Channels.newChannel(website.openStream());
         FileOutputStream fos = new FileOutputStream(adress);
---
+++ src/com/github/charslush/second/consts/Constants.java
@@ -0,0 +1,7 @@
+package com.github.charslush.second.consts;
+
+public interface Constants {
+    String SLASH = "/";
+    String DOT = ".";
+    String DESTINATION_DIR = "~/";
+}
